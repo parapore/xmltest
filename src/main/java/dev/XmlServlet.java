7304c907c967f49fdf9126176f242489fb484b24
parapore <eriglai577@gmail.com>
2019-12-27 10:47:36 +0900

    JSPからサーブレットにリクエストを飛ばすテスト

--- src/main/java/dev/XmlServlet.java
+++ src/main/java/dev/XmlServlet.java
@@ -2,6 +2,7 @@ package dev;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -15,28 +16,40 @@ import javax.servlet.http.HttpServletResponse;
 public class XmlServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public XmlServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public XmlServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletOutputStream out = response.getOutputStream();
-        out.write("Hello World on Heroku".getBytes());
-        out.flush();
-        out.close();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// フォワード
+		RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+		rd.forward(request, response);
+
+		String s = request.getParameter("action");
+		if(s != null) {
+			ServletOutputStream out = response.getOutputStream();
+			out.write("Hello World on Heroku".getBytes());
+			out.flush();
+			out.close();
+		}
+
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false" %>
+<!DOCTYPE html>
 <html>
+<head>
+<meta charset="UTF-8">
+<style type="text/css">
+<!--
+   div.ta0 {text-align: left;}
+   div.ta1 {text-align: center;}
+   div.ta2 {text-align: right;}
+   div.ta3 {text-align: justify;}
+-->
+</style>
+<title>いいね！！！</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<div class="ta1">
+<p><a href="/XmlServlet?action=click"><font size="5">クリック！</font></a></p>
+<p><font size="5"><b>いいね！：${yoine.yoineCount}</b></font></p>
+</div>
 </body>
 </html>
