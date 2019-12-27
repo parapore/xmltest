da6b855815d3e31806477161aaa4baf80d708337
parapore <eriglai577@gmail.com>
2019-12-27 10:09:13 +0900

    change annotation

--- src/main/java/dev/XmlServlet.java
+++ src/main/java/dev/XmlServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class XmlServlet
  */
+@WebServlet("/XmlServlet")
 public class XmlServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
