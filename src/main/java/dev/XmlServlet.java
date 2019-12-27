51b6ae6180b2bcbd4cbae20164c4097e1ca62cc2
parapore <eriglai577@gmail.com>
2019-12-27 10:28:33 +0900

    return all

--- src/main/java/dev/XmlServlet.java
+++ src/main/java/dev/XmlServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class XmlServlet
  */
-@WebServlet("/XmlServlet")
+
 public class XmlServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
